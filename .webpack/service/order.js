f4180fdebc8edfe137df15f6d88dbfc04fe75e46
Tuan Mai <perhuman@gmail.com>
2019-06-22 19:08:59 +0700

    Basic create order feature

--- .webpack/service/order.js
+++ .webpack/service/order.js
@@ -115,35 +115,102 @@ var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 __webpack_require__(/*! source-map-support/register */ "source-map-support/register");
 
+var _uuid = __webpack_require__(/*! uuid */ "uuid");
+
+var _uuid2 = _interopRequireDefault(_uuid);
+
+var _awsSdk = __webpack_require__(/*! aws-sdk */ "aws-sdk");
+
+var _awsSdk2 = _interopRequireDefault(_awsSdk);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var dynamoDb = new _awsSdk2.default.DynamoDB.DocumentClient();
+
 var addToCard = function () {
-  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(event) {
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(event, context, callback) {
+    var data, params, headers, _response, response;
+
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            return _context.abrupt("return", {
-              statusCode: 200,
-              body: (0, _stringify2.default)({
-                message: "Go Serverless v1.0! Your function executed successfully!"
-              }, null, 2)
-            });
+            // Request body is passed in as a JSON encoded string in 'event.body'
+            data = JSON.parse(event.body);
+            params = {
+              TableName: 'orders',
+              // 'Item' contains the attributes of the item to be created
+              // - 'userId': user identities are federated through the
+              //             Cognito Identity Pool, we will use the identity id
+              //             as the user id of the authenticated user
+              // - 'noteId': a unique uuid
+              // - 'content': parsed from request body
+              // - 'attachment': parsed from request body
+              // - 'createdAt': current Unix timestamp
+              Item: {
+                userId: data.userId,
+                orderId: _uuid2.default.v1(),
+                createdAt: Date.now()
+              }
+            };
+            headers = {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true
+            };
+            _context.prev = 3;
+            _context.next = 6;
+            return dynamoDb.put(params);
 
-          case 1:
-          case "end":
+          case 6:
+            _context.next = 12;
+            break;
+
+          case 8:
+            _context.prev = 8;
+            _context.t0 = _context['catch'](3);
+            _response = {
+              statusCode: 500,
+              headers: headers,
+              body: (0, _stringify2.default)({ status: false })
+            };
+
+            callback(null, _response);
+
+          case 12:
+            response = {
+              statusCode: 200,
+              headers: headers,
+              body: (0, _stringify2.default)(params.Item)
+            };
+
+            callback(null, response);
+
+          case 14:
+          case 'end':
             return _context.stop();
         }
       }
-    }, _callee, undefined);
+    }, _callee, undefined, [[3, 8]]);
   }));
 
-  return function addToCard(_x) {
+  return function addToCard(_x, _x2, _x3) {
     return _ref.apply(this, arguments);
   };
 }();
 
+// eslint-disable-next-line
 exports.addToCard = addToCard;
+
+/***/ }),
+
+/***/ "aws-sdk":
+/*!**************************!*\
+  !*** external "aws-sdk" ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("aws-sdk");
 
 /***/ }),
 
@@ -188,6 +255,17 @@ module.exports = require("babel-runtime/regenerator");
 /***/ (function(module, exports) {
 
 module.exports = require("source-map-support/register");
+
+/***/ }),
+
+/***/ "uuid":
+/*!***********************!*\
+  !*** external "uuid" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("uuid");
 
 /***/ })
 
--- order.js
+++ order.js
@@ -1,14 +1,52 @@
-const addToCard = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!"
-      },
-      null,
-      2
-    )
+import uuid from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const addToCard = async (event, context, callback) => {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: 'orders',
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: data.userId,
+      orderId: uuid.v1(),
+      createdAt: Date.now(),
+    },
   };
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  };
+
+  try {
+    await dynamoDb.put(params);
+  } catch (err) {
+    const response = {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ status: false }),
+    };
+    callback(null, response);
+  }
+
+  const response = {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(params.Item),
+  };
+  callback(null, response);
 };
 
+// eslint-disable-next-line
 export { addToCard };

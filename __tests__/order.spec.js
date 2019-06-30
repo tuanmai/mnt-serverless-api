f8ab965519c1e33ddfafcc0939d2c258488c39fb
Tuan Mai <perhuman@gmail.com>
2019-06-30 11:03:56 +0700

    Add init test for check pending order

--- __tests__/order.spec.js
+++ __tests__/order.spec.js
@@ -3,10 +3,48 @@ import AWS from "aws-sdk-mock";
 
 import { addToCard } from "../order";
 import Order from "../db/order";
-let orders = [];
+
+jest.mock("../db/order");
 
 describe("Add to card", () => {
   describe("There is no pending order", () => {
-    it("creates new order", () => {});
+    it("creates new order", async () => {
+      const queryMock = { success: true, data: { Count: 0, Items: [] } };
+      const createMock = {
+        success: true,
+        data: { id: "order_1", orderStatus: "pending" }
+      };
+      const event = {
+        body: JSON.stringify({ userId: "123" })
+      };
+      Order.query.mockReturnValue(Promise.resolve(queryMock));
+      Order.put.mockReturnValue(Promise.resolve(createMock));
+      const result = await addToCard(event);
+      const bodyData = JSON.parse(result.body);
+      expect(bodyData.orderStatus).toEqual("pending");
+      expect(bodyData.id).toEqual("order_1");
+      expect(result.statusCode).toEqual(200);
+    });
+  });
+
+  describe("There is a pending order", () => {
+    it("creates new order", async () => {
+      const queryMock = {
+        success: true,
+        data: { Count: 1, Items: [{ id: "order_1" }] }
+      };
+      const createMock = {
+        success: true,
+        data: { id: "order_2", orderStatus: "pending" }
+      };
+      const event = {
+        body: JSON.stringify({ userId: "123" })
+      };
+      Order.query.mockReturnValue(Promise.resolve(queryMock));
+      Order.put.mockReturnValue(Promise.resolve(createMock));
+      const result = await addToCard(event);
+      const bodyData = JSON.parse(result.body);
+      expect(bodyData.id).toEqual("order_1");
+    });
   });
 });
